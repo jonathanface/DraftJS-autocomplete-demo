a18248ad9f123512ba0ecfde3a71aca1df7f050f
Jonathan Face <=>
2020-02-03 01:55:52 -0500

    almost done

--- static/js/src/Main.jsx
+++ static/js/src/Main.jsx
@@ -1,140 +1,315 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { EditorState, Editor, CompositeDecorator, convertToRaw } from 'draft-js';
+import { EditorState, Editor, SelectionState, CompositeDecorator, Modifier } from 'draft-js';
 
-const names = ['John Smith', 'Jim Avery', 'Jonas Salk'];
+const HANDLE_REGEX = /\@[\w\s]+/gi;
+const HASHTAG_REGEX = /\#[\w\u0590-\u05ff]+/gi;
+const IDEA_REGEX = /\<>[\w\u0590-\u05ff]+/gi;
+const FINALIZE_REGEX = /\%%%[\w\s]+%%%/gi;
+
+const names = ['jonas salk', 'jim avery', 'bob jenkins'];
+var suggestions = [];
 
 export class Main extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    const compositeDecorator = new CompositeDecorator([
+      {
+        strategy: this.handleStrategy.bind(this),
+        component: this.HandleSpan,
+      },
+      {
+        strategy: this.hashtagStrategy.bind(this),
+        component: this.HashtagSpan,
+      },
+      {
+        strategy: this.ideaStrategy.bind(this),
+        component: this.IdeaSpan,
+      },
+      {
+        strategy: this.handleFinalize.bind(this),
+        component: this.FinalizeSpan,
+      },
+    ]);
+
     this.state = {
-      searchingNames:false,
-      editorState: EditorState.createEmpty()
+      editorState: EditorState.createEmpty(compositeDecorator),
+      tagIndex:0,
+      browsingSuggestions:false,
+      currentBlock:null
+    };
+
+    this.focus = () => this.refs.editor.focus();
+    this.onChange = (editorState) => this.setState({editorState});
+    
+  }
+  
+  onDownArrow(event) {
+    event.preventDefault();
+    if (suggestions.length) {
+      if (this.state.tagIndex > suggestions.length-1) {
+        this.setState({
+          tagIndex:0,
+          browsingSuggestions:false
+        });
+        this.focus();
+        return;
+      }
+      this.state.browsingSuggestions = true;
+      let currSpan = document.querySelectorAll('.searchResults li')[this.state.tagIndex];
+      currSpan.focus();
+      this.state.tagIndex++;
+      
     }
   }
   
-  addSelections(matches) {
-    
-    
+  onUpArrow(event) {
+    event.preventDefault();
+    if (suggestions.length) {
+      this.state.tagIndex-=2;
+      if (this.state.tagIndex < 0) {
+        this.setState({
+          tagIndex:0,
+          browsingSuggestions:false
+        });
+        this.focus();
+        return;
+      }
+      this.state.browsingSuggestions = true;
+      let currSpan = document.querySelectorAll('.searchResults li')[this.state.tagIndex];
+      currSpan.focus();
+      this.state.tagIndex--;
+    }
+  }
+  
+  onEnter(event) {
+    event.preventDefault();
+    if (this.state.browsingSuggestions) {
+      this.setState({
+        tagIndex:0,
+        browsingSuggestions:false
+      });
+      this.focus();
+      this.clearSelections();
+      let text = event.target.innerText;
+      
+      let selectionState = this.state.editorState.getSelection();
+      var anchorKey = selectionState.getAnchorKey();
+      var currentContent = this.state.editorState.getCurrentContent();
+      var currentContentBlock = currentContent.getBlockForKey(anchorKey);
+      console.log(anchorKey, currentContent);
+      console.log('curr', currentContent.anchorOffset, currentContentBlock.focusOffset);
+      currentContent = Modifier.replaceText(
+        currentContent,
+        new SelectionState({
+          anchorKey: currentContentBlock.getKey(),
+          anchorOffset: 0,
+          focusKey: currentContentBlock.getKey(),
+          focusOffset: currentContentBlock.text.length
+        }),
+        '%%%'+text+'%%%'
+      );
+      this.clearSelections();
+      this.setState({
+        editorState: EditorState.push(
+          this.state.editorState,
+          currentContent,
+        )
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div onClick={this.focus}>
+          <Editor
+            onDownArrow={this.onDownArrow.bind(this)}
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="Write something..."
+            ref="editor"
+            />
+        </div>
+        <div className="searchResults"></div>
+      </div>
+    );
+  }
+  
+  handleSuggestionPress(event, block) {
+    console.log('blk', this.state.currentBlock);
+    event.preventDefault();
+    console.log('pressed span');
+    console.log('evt', event);
+    switch(event.key) {
+      case 'ArrowDown':
+      case 'Tab':
+        this.onDownArrow(event);
+        break;
+      case 'ArrowUp':
+        this.onUpArrow(event);
+        break;
+      case 'Enter':
+        this.onEnter(event);
+        break;
+    }
+  }
+  
+  addSelections(type) {
     document.querySelector('.searchResults').innerHTML = '';
     let ul = document.createElement('ul');
-    matches.forEach(match => {
+    let self = this;
+    suggestions.forEach(function(match, index) {
       let li = document.createElement('li');
+      li.tabIndex = index;
+      li.onkeydown = self.handleSuggestionPress.bind(self);
       li.innerHTML = match;
       ul.appendChild(li);
     });
     document.querySelector('.searchResults').appendChild(ul);
-    let highlight = document.querySelector('.search-highlight');
+    let highlight = document.querySelector(type);
     if (highlight) {
-      console.log(highlight.offsetLeft);
-      document.querySelector('.searchResults').style.left = highlight.offsetLeft + 25 + 'px';
+      document.querySelector('.searchResults').style.left = highlight.offsetLeft + 27 + 'px';
     }
   }
   
   clearSelections() {
     document.querySelector('.searchResults').innerHTML = '';
   }
+  
+  handleFinalize(contentBlock, callback, contentState) {
+    const text = contentBlock.getText();
+    let matchArr, start, end;
+    while ((matchArr = FINALIZE_REGEX.exec(text)) !== null) {
+      start = matchArr.index;
+      end = start + matchArr[0].length;
+    }
 
-  onChange = (editorState) => {
-    let self = this;
-    this.setState({
-      editorState:editorState
+    const contentStateWithEntity = contentState.createEntity('LINK', 'IMMUTABLE', {
+      url: 'http://www.zombo.com',
     });
-    const blocks = convertToRaw(editorState.getCurrentContent()).blocks;
-    const text = blocks.map(block => (!block.text.trim() && '\n') || block.text).join('\n');
-    if (text[text.length-1] == '@') {
-      this.setState({
-        searchingNames:true
-      });
-    }
-    if (this.state.searchingNames && text.length) {
-      //let start = text.lastIndexOf('@');
-      var selectionState = editorState.getSelection();
-      var anchorKey = selectionState.getFocusKey();
-      var currentContent = editorState.getCurrentContent();
-      var currentContentBlock = currentContent.getBlockForKey(anchorKey);
-      var end = selectionState.getEndOffset();
-      let start;
-      
-      for (let i = end; i >= 0; i--) {
-        if (text[i] == '@') {
-          start = i;
-          break;
-        }
-      }
-      console.log('start', start, end);
-      var substr = currentContentBlock.getText().slice(start, start+end);
-      //var substr = text.substring(start, end);
-      console.log('sub', substr);
-      let regex = new RegExp(substr, 'ig');
-      let matches = [];
-      names.forEach(function (name, index) {
-        let search = ('@' + name).match(regex);
-        if (search) {
-          matches.push(names[index]);
-        }
-      });
-      if (matches.length) {
-        if (matches.length == 1 && matches[0].length+1 == end) {
-          self.setState({
-            searchingNames:false,
-            editorState: EditorState.set(editorState, { decorator: self.generateDecorator(start, substr.length, this.FinalizeHighlight)})
-          });
-          self.clearSelections();
-        } else {
-          
-          self.setState({
-            editorState: EditorState.set(editorState, { decorator: self.generateDecorator(start, substr.length, this.SearchHighlight)})
-          }, function() {
-            setTimeout(function() {
-              self.addSelections(matches), 400
-          })
-          });
-          //self.addSelections(matches);
-        }
-      } else {
-        self.clearSelections();
-        matches = [];
-      }
-      
-    }
-    
-  }
-
-  SearchHighlight = (props) => (
-    <span className="search-highlight">{props.children}</span>
-  );
-  FinalizeHighlight = (props) => (
-    <span className="found-box">{props.children}</span>
-  );
-
-
-  generateDecorator = (start, end, type) => {
-    return new CompositeDecorator([{
-      strategy: (contentBlock, callback) => {
-        //this.findWithRegex(regex, contentBlock, callback, state, finalized);
-        this.echoBack(start, end, callback);
-      },
-      component: type
-    }])
-  }
-  
-  // Pointless but it seems like Draft decorators require a callback
-  echoBack = (start, end, callback) => {
-    console.log('calling back...', start, 'to', start+end);
-    callback(start, start + end);
-    
-  }
-  
-  render() {
-    return (
-      <div>
-        <Editor
-          editorState={this.state.editorState}
-          onChange={this.onChange}
-        />
-        <div className="searchResults"></div>
-      </div>
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    const contentStateWithLink = Modifier.applyEntity(
+      contentStateWithEntity,
+      new SelectionState({
+        anchorKey: contentBlock.getKey(),
+        anchorOffset: 0,
+        focusKey: contentBlock.getKey(),
+        focusOffset: 2
+      }),
+      entityKey,
     );
+    EditorState.push(this.state.editorState, {
+      currentContent: contentStateWithLink,
+    });
+    callback(start, end);
+
+    //this.findWithRegex(FINALIZE_REGEX, contentBlock, callback);
   }
+  
+  handleStrategy(contentBlock, callback, contentState) {
+    const text = contentBlock.getText();
+    let start, matchArr, matches = [];
+    let textSlice = '';
+    this.setState({
+      suggestions:[],
+      tagIndex:0
+    });
+    let self = this;
+    while ((matchArr = HANDLE_REGEX.exec(text)) !== null) {
+      start = matchArr.index;
+      let end = start + matchArr[0].length;
+      textSlice = text.slice(start+1, end);
+      let regex = new RegExp('^'+textSlice, 'ig');
+      
+      names.forEach(function (name, index) {
+        let search = name.match(regex);
+        console.log(search);
+        if (search) {
+          matches.push(name);
+        }
+      });
+    }
+    if (matches.length) {
+      suggestions = matches;
+      this.setState({
+        currentBlock:contentBlock
+      });
+      this.addSelections('.handle');
+    } else {
+      this.setState({
+        currentBlock:null
+      });
+      this.clearSelections();
+    }
+    this.findWithRegex(HANDLE_REGEX, contentBlock, callback);
+  }
+
+  hashtagStrategy(contentBlock, callback, contentState) {
+    this.findWithRegex(HASHTAG_REGEX, contentBlock, callback);
+  }
+
+  ideaStrategy(contentBlock, callback, contentState) {
+    this.findWithRegex(IDEA_REGEX, contentBlock, callback);
+  }
+
+
+
+  findWithRegex(regex, contentBlock, callback) {
+    const text = contentBlock.getText();
+    let matchArr, start;
+    while ((matchArr = regex.exec(text)) !== null) {
+      start = matchArr.index;
+      console.log(matchArr);
+      callback(start, start + matchArr[0].length);
+    }
+  }
+
+  HandleSpan = (props) => {
+    return (
+      <span
+        className="handle"
+        data-offset-key={props.offsetKey}
+        >
+        {props.children}
+      </span>
+    );
+  };
+
+  HashtagSpan = (props) => {
+    return (
+      <span
+        className="hashtag"
+        data-offset-key={props.offsetKey}
+        >
+        {props.children}
+      </span>
+    );
+  };
+
+  IdeaSpan = (props) => {
+    return (
+      <span
+        className="idea"
+        data-offset-key={props.offsetKey}
+        >
+        {props.children}
+      </span>
+    );
+  };
+  
+  FinalizeSpan = (props) => {
+    return (
+      <span
+        className="final"
+        data-offset-key={props.offsetKey}
+        >
+        {props.children}
+      </span>
+    );
+  };
+
 }
+
+
+
